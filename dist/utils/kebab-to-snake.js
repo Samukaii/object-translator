ee5e4323129c69507e1f835795e97fc692abb444
samuel <samuel.veras@joinin.com.br>
2024-04-17 10:52:54 -0300

    .gitignore adjust

--- dist/utils/kebab-to-snake.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kebabToSnake = void 0;
-const kebabToSnake = (kebabCaseString) => kebabCaseString.replace(/-/g, '_');
-exports.kebabToSnake = kebabToSnake;
-//# sourceMappingURL=kebab-to-snake.js.map
